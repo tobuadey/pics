4875a4a965c0f5ac5247035b83f5dd1a6885ffee
tobuadey <144479641+tobuadey@users.noreply.github.com>
2026-02-07 02:43:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 st.set_page_config(
     page_title="💘",
     page_icon="💖",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="collapsed"
 )
 
